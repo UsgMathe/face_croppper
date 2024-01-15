73b32ff076b18482b3b64b0477703c3af2d61b0d
Matheus Gabriel Valenga <64933310+UsgMathe@users.noreply.github.com>
2024-01-15 09:19:21 -0300

    Update index.js

--- src/services/api/index.js
+++ src/services/api/index.js
@@ -1,13 +1,5 @@
 import axios from "axios"
-
-// Vitor:
-// const baseURL = process.env.BASE_URL || "http://192.168.51.213:5000/"
-
-// Gabriel celular:
-// const baseURL = process.env.BASE_URL || "http://192.168.43.69:5000/"
-
-// Gabriel PC:
-const baseURL = process.env.BASE_URL || "http://192.168.50.60:5000/"
+const baseURL = process.env.BASE_URL || "http://localhost:5000/"
 
 export const api = axios.create({
   baseURL
